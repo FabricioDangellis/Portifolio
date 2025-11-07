8007d30c3271b58e7601901f0280cb968907c367
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2025-11-07 10:46:32 -0300

    ADD: Adicionando seção de Serviços

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,9 @@
-import { ArrowDownToLine } from "lucide-react";
+import {
+  ArrowDownToLine,
+  Building2,
+  PanelsTopLeft,
+  Server,
+} from "lucide-react";
 import NavBar from "../components/NavBar";
 import Perfil from "../assets/Perfil.png";
 import Imagem1 from "../assets/turned-gray-laptop-computer.jpg";
@@ -57,7 +62,7 @@ export default function Home() {
 
       <div
         id="sobre"
-        className="relative isolate overflow-hidden bg-gray-900 px-6 py-10 sm:flex lg:overflow-visible lg:px-0"
+        className="relative isolate overflow-hidden bg-gray-900 px-6 py-10 sm:flex lg:overflow-visible lg:px-0 pb-20 sm:pb-0"
       >
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-6 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-0">
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
@@ -71,9 +76,9 @@ export default function Home() {
                   <span className="text-indigo-400">FullStack</span>
                 </h1>
                 <p className="mt-6 text-xl/8 text-gray-300">
-                  Me chamo <strong>Fabrício D'Angellis</strong>. Sou um desenvolvedor fullstack
-                  júnior com experiência em React, TypeScript, NodeJs e
-                  PostgreSQL
+                  Me chamo <strong>Fabrício D'Angellis</strong>. Sou um
+                  desenvolvedor fullstack júnior com experiência em React,
+                  TypeScript, NodeJs e PostgreSQL
                 </p>
               </div>
             </div>
@@ -109,6 +114,64 @@ export default function Home() {
               src={Imagem1}
               className="h-90 max-w-none rounded-xl bg-gray-800 shadow-xl ring-1 ring-white/10 sm:h-140"
             />
+          </div>
+        </div>
+      </div>
+
+      <div id="servicos" className="bg-gray-900 min-h-screen flex items-center pb-20 sm:pb-0">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:text-center">
+            <h2 className="text-base/7 font-semibold text-indigo-400">
+              Serviços
+            </h2>
+            <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl lg:text-balance">
+              <span className="text-indigo-400">Desenvolvimento</span> de
+              soluções
+            </p>
+            <p className="mt-6 text-lg/8 text-gray-300">
+              Desenvolvo soluções completas para web, unindo front-end moderno e
+              back-end seguro. Meu foco é criar interfaces rápidas, funcionais e
+              experiências digitais que realmente geram valor.
+            </p>
+          </div>
+          <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
+            <dl className="grid grid-cols-1 gap-x-15 gap-y-16 text-center lg:grid-cols-3">
+              <div className="mx-auto flex items-center max-w-xs flex-col gap-y-4">
+                <PanelsTopLeft className="w-15 h-15 text-indigo-400" />
+                <dd className="text-2xl font-semibold tracking-tight text-white sm:text-3xl">
+                  Landing Pages
+                </dd>
+                <dt className="text-base/7 text-gray-400">
+                  Páginas rápidas, responsivas e otimizadas para conversão.
+                  Integração com formulários, automações e ferramentas de
+                  marketing. Código limpo, leve e focado em performance.
+                </dt>
+              </div>
+
+              <div className="mx-auto flex items-center max-w-xs flex-col gap-y-4">
+                <Building2 className="w-15 h-15 text-indigo-400" />
+                <dd className="text-2xl font-semibold tracking-tight text-white sm:text-3xl">
+                  Sites Institucionais
+                </dd>
+                <dt className="text-base/7 text-gray-400">
+                  Desenvolvimento de sites profissionais, modernos e acessíveis.
+                  Navegação fluida, design responsivo e integração com APIs e
+                  áreas administrativas quando necessário.
+                </dt>
+              </div>
+
+              <div className="mx-auto flex items-center max-w-xs flex-col gap-y-4">
+                <Server className="w-15 h-15 text-indigo-400" />
+                <dd className="text-2xl font-semibold tracking-tight text-white sm:text-3xl">
+                  Back-end e APIs
+                </dd>
+                <dt className="text-base/7 text-gray-400">
+                  Criação de APIs seguras e escaláveis usando Node.js.
+                  Autenticação, banco de dados, upload de arquivos, serviços
+                  externos e arquitetura bem estruturada.
+                </dt>
+              </div>
+            </dl>
           </div>
         </div>
       </div>

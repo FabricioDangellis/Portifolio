1fa5597aa72961d997db2ecdf66c240e1a37132f
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2025-11-09 21:21:09 -0300

    ADD: Adcionando rodapé da página

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import {
   Mail,
   PanelsTopLeft,
   Server,
+  SquareCode,
 } from "lucide-react";
 import NavBar from "../components/NavBar";
 import Perfil from "../assets/Perfil.png";
@@ -15,6 +16,8 @@ import TaskBoard from "../assets/Imagem2.png";
 import Projeto from "../components/Projeto";
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
+
   const projetos = [
     {
       titulo: "Trajetórias",
@@ -298,7 +301,7 @@ export default function Home() {
               <div className="mt-2.5">
                 <input
                   id="name"
-                  name="name"
+                  name="Nome"
                   type="text"
                   autoComplete="given-name"
                   className="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500"
@@ -332,7 +335,7 @@ export default function Home() {
               <div className="mt-2.5">
                 <textarea
                   id="message"
-                  name="message"
+                  name="Mensagem"
                   rows={4}
                   className="block w-full rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500"
                   defaultValue={""}
@@ -350,6 +353,71 @@ export default function Home() {
           </div>
         </form>
       </div>
+
+      <footer className="w-full bg-gray-900  text-gray-400 py-10 px-6 flex flex-col items-center gap-6">
+        <div className="flex flex-col md:flex-row justify-between items-center w-full gap-6 md:gap-0">
+          <div className="flex items-center gap-3">
+            <a
+              href="#"
+              className="-m-1.5 p-1.5 text-white flex items-center gap-2"
+            >
+              <SquareCode className="h-8 w-auto text-indigo-500" />
+              Fabrício D'Angellis
+            </a>
+          </div>
+
+          <nav className="flex gap-6 flex-wrap justify-center text-sm md:text-base">
+            <a href="#topo" className="hover:text-white transition">
+              Home
+            </a>
+            <a href="#sobre" className="hover:text-white transition">
+              Sobre
+            </a>
+            <a href="#servicos" className="hover:text-white transition">
+              Serviços
+            </a>
+            <a href="#projetos" className="hover:text-white transition">
+              Projetos
+            </a>
+            <a href="#contato" className="hover:text-white transition">
+              Contato
+            </a>
+          </nav>
+
+          <div className="flex flex-col gap-5 text-xl">
+            <a
+              href="https://github.com/FabricioDangellis"
+              target="_blank"
+              className="flex gap-2 items-center"
+            >
+              <div className="bg-indigo-400 rounded-lg w-7 h-7 flex items-center justify-center">
+                <Github className="text-white w-5 h-auto" />
+              </div>
+              <span className="group-hover:text-white text-sm">
+                Fabrício D'Angellis
+              </span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/fabrício-d-angellis-22559b25a"
+              target="_blank"
+              className="flex gap-2 items-center"
+            >
+              <div className="bg-indigo-400 rounded-lg w-7 h-7 flex items-center justify-center">
+                <Linkedin className="text-white w-5 h-auto" />
+              </div>
+              <span className="group-hover:text-white text-sm">
+                Fabrício D'Angellis
+              </span>
+            </a>
+          </div>
+        </div>
+
+        <div className="w-full h-px bg-gray-700 mt-4 mb-2"></div>
+
+        <p className="text-sm text-gray-500">
+          © {currentYear} Fabrício D’Angellis. Todos os direitos reservados.
+        </p>
+      </footer>
     </section>
   );
 }
